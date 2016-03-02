791fb486ae42ed0460967ee63a08045ed4cc4243
Francis Park <francis.park@greenpeace.org>
2016-03-02 14:24:59 +1300

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,445 @@
+$(document).ready(function() {
+
+    $(".btn").hover(function() {
+        var hovSrc = $(this).data('src');
+        var normSrc = $(this).attr('src');
+        $(this).attr('src', hovSrc);
+        $(this).data('src', normSrc);
+    }, function() {
+        var normSrc = $(this).data('src');
+        var hovSrc = $(this).attr('src');
+        $(this).attr('src', normSrc);
+        $(this).data('src', hovSrc);
+    })
+
+    //Sharing
+    if (addthis) {
+        addthis.toolbox('#toolbox', {}, {
+            url: "http://www.greenpeace.org/catssavetigers",
+            title: 'Greenpeace - Cats Save Tigers',
+            templates: {
+			twitter: 'The biggest cats online save the biggest cats in the wild. Upload your cat pic and join the cause! ROAR! - {{url}}'
+            }
+        });
+    }
+
+
+    //Youtube video
+    $('#videoimage').click(function() {
+        $('#videoimage img').fadeOut();
+        $('#videoimage').append('<div class="video-container"><iframe title="YouTube video player" width="' + $(videoimage).innerWidth() + 'px" height="' + $(videoimage).innerHeight() + 'px" src="//www.youtube.com/embed/FOLHV3hdDPw?autoplay=1" frameborder="0" allowfullscreen></iframe></div>');
+        _gaq.push(['_trackEvent', "Cat saves Tiger", "YouTube watched"]);
+        _gaq.push(['account2._trackEvent', "Cat saves Tiger", "YouTube watched"]);
+        _gaq.push(['account3._trackEvent', "Cat saves Tiger", "YouTube watched"]);
+    });
+    $(window).resize(function() {
+        $('.video-container iframe').attr('width', $(videoimage).innerWidth() + 'px');
+        $('.video-container iframe').attr('height', $(videoimage).innerHeight() + 'px');
+        $('#showerror').css('display', 'none');
+        $('#shareSuccess').css('display', 'none');
+    })
+
+    // Get number of Memes
+    var global_signups;
+    var liveCounter = function() {
+        var getSigned = $.get(
+            "//moon.greenpeace.org/c/print.php?a=tigerday",
+            function(signed) {
+                global_signups = parseInt(signed);
+            }
+        );
+        $.when(getSigned).done(function() {
+            $('.num-sofar').html(global_signups);
+        });
+    };
+    setInterval(function() {
+        try {
+            liveCounter();
+        } catch (e) { /* . */ }
+    }, 1000);
+
+    //
+
+    //Reset canvas if screen is less than 350px;
+    function responsiveCanvas() {
+        if ($('body').outerWidth() < 370 && $('body').outerWidth() >= 321) {
+            $('canvas').attr('width', '275');
+            $('canvas').attr('height', '275');
+            $('.image-holder').css('width', '275px');
+            $('.image-holder').css('height', '275px');
+        }
+        if ($('body').outerWidth() < 321) {
+            $('canvas').attr('width', '250');
+            $('canvas').attr('height', '250');
+            $('.image-holder').css('width', '250px');
+            $('.image-holder').css('height', '250px');
+        }
+    }
+    responsiveCanvas();
+    //
+
+    function shuffle(array) {
+        var tmp, current, top = array.length;
+        if (top)
+            while (--top) {
+                current = Math.floor(Math.random() * (top + 1));
+                tmp = array[current];
+                array[current] = array[top];
+                array[top] = tmp;
+            }
+        return array;
+    }
+
+    // Get images for gallery
+    $.ajax({
+        type: 'GET',
+        url: 'assets/js/catImages.json',
+        dataType: 'json',
+        success: function(data) {
+
+            for (var a = [], i = 0; i < data.catImages.length - 1; ++i) a[i] = i;
+            a = shuffle(a);
+
+            $('.gallery-container img').each(function(index) {
+                $(this).attr('src', data.catImages[a[index]].src)
+            });
+            $('.gallery-container a').each(function(index) {
+                $(this).attr('href', data.catImages[a[index]].link)
+            });
+
+            $('#sofarcontent').fadeIn();
+        }
+    })
+    //
+
+    $('.upload-image').show();
+    $('.upload-success').hide();
+
+    $('.upload').click(function() {
+        $('#uploadimage').click();
+    });
+
+    $('.submit').click(function() {
+
+        var errorCount = 0;
+        $('.left-col .error').removeClass('error');
+
+        if ($('#message').val() === '') {
+            errorCount++;
+            $('.message-p label').addClass('error');
+        }
+        if ($('#catname').val() === '') {
+            errorCount++;
+            $('.catname-p label').addClass('error');
+        }
+        if ($('#uploadimage').val() === '') {
+            errorCount++;
+            $('#furry-label').addClass('error');
+        }
+
+        if (errorCount == 0) {
+            $('.upload-image').hide();
+            $('.upload-success').show();
+            if ($('#showerror').css('display') !== 'none') $('#form').css('height', $('#form').outerHeight() - 40 + "px")
+            $('#showerror').css('display', 'none');
+            _gaq.push(['_trackEvent', "Cat saves Tiger", "Meme created"]);
+            _gaq.push(['account2._trackEvent', "Cat saves Tiger", "Meme created"]);
+            _gaq.push(['account3._trackEvent', "Cat saves Tiger", "Meme created"]);
+            $("body").append("<img src='//moon.greenpeace.org/c/?a=tigerday' width='' height='' alt=''>");
+        } else {
+            if ($('#showerror').css('display') !== 'block') $('#form').css('height', $('#form').outerHeight() + 40 + "px")
+            $('#showerror').css('display', 'block');
+            _gaq.push(['_trackEvent', "Cat saves Tiger", "Meme submitted with errors"]);
+            _gaq.push(['account2._trackEvent', "Cat saves Tiger", "Meme submitted with errors"]);
+            _gaq.push(['account3._trackEvent', "Cat saves Tiger", "Meme submitted with errors"]);
+        }
+
+    });
+
+    $('.facebook-btn').click(function(event) {
+        postCanvasToFacebook();
+        _gaq.push(['_trackEvent', "Cat saves Tiger", "Shared Meme to Facebook"]);
+        _gaq.push(['account2._trackEvent', "Cat saves Tiger", "Shared Meme to Facebook"]);
+        _gaq.push(['account3._trackEvent', "Cat saves Tiger", "Shared Meme to Facebook"]);
+    });
+
+    $('.twitter-btn').click(function(event) {
+        postCanvasToTwitter();
+        _gaq.push(['_trackEvent', "Cat saves Tiger", "Shared Meme to Twitter"]);
+        _gaq.push(['account2._trackEvent', "Cat saves Tiger", "Shared Meme to Twitter"]);
+        _gaq.push(['account3._trackEvent', "Cat saves Tiger", "Shared Meme to Twitter"]);
+    });
+
+
+});
+
+function CatInfo() {
+    $('.catnametext').html($('#catname').val());
+    $('.messagetext').html($('#message').val());
+}
+
+$(function() {
+
+    var cvs = $("canvas"),
+        cvsWidth = cvs.width(),
+        cvsHeight = cvs.height(),
+        ctx = cvs[0].getContext("2d"),
+        top = $("#message"),
+        bottom = $("#catname");
+
+    var img = this;
+    var scaledWidth = 330;
+    var scaledHeight = 330;
+    var posY = 0;
+    var posX = 0;
+    var width = img.naturalWidth;
+    var height = img.naturalHeight;
+
+    function writeCaption(text, y) {
+        var size = 35;
+
+        do {
+            size--;
+            ctx.font = size + 'px Impact, HelveticaNeue-CondensedBlack';
+            ctx.lineWidth = size / 15;
+        } while (ctx.measureText(text).width > cvsWidth)
+
+        ctx.fillText(text, cvsWidth / 2, y);
+        ctx.strokeText(text, cvsWidth / 2, y);
+
+    }
+
+    // Setup basic canvas settings
+    $.extend(ctx, {
+        strokeStyle: "#000000",
+        textAlign: 'center',
+        fillStyle: "#ffffff",
+        lineCap: "round"
+    })
+
+    $("<img />")
+        .load(function() {
+
+            var img = this;
+
+            $(document.body).on("keyup", function() {
+                var topText = top.val(),
+                    bottomText = bottom.val();
+                topText = topText.toUpperCase();
+
+                ctx.clearRect(0, 0, cvsWidth, cvsHeight);
+                ctx.drawImage(img, 0, 0, cvsWidth, cvsHeight);
+                ctx.textBaseline = 'top';
+                writeCaption(topText, 10);
+
+                CatInfo();
+
+            }).trigger("keyup");
+
+        })
+        .attr("src", "assets/images/empty-photo.png");
+
+    $('#uploadimage').change(function(e) {
+        /* Use the FileReader */
+        var file = e.target.files[0],
+            imageType = /image.*/;
+
+        if (!file.type.match(imageType))
+            return;
+
+        var reader = new FileReader();
+        reader.onload = fileOnload;
+        reader.readAsDataURL(file);
+    });
+
+    function fileOnload(event) {
+        var img = new Image();
+        img.src = event.target.result;
+
+        $("<img />").attr('src', img.src).load(function() {
+
+            var img = this;
+
+            scaledWidth = $('canvas').innerWidth();
+            scaledHeight = $('canvas').innerHeight();
+            width = img.naturalWidth;
+            height = img.naturalHeight;
+            posY = 0;
+            posX = 0;
+            var drag = false;
+            var draggedDist = 0;
+
+            aspectRat = width / height;
+
+            if (height > width) {
+                drag = 'upDown';
+                if (height > $('canvas').innerHeight()) {
+                    scaledWidth = $('canvas').innerWidth();
+                    scaledHeight = scaledWidth / aspectRat;
+                    posY = Math.ceil((scaledHeight - scaledWidth) / 2);
+                } else {
+                    scaledHeight = $('canvas').innerHeight();
+                    scaledWidth = scaledHeight * aspectRat;
+                    posX = Math.ceil((scaledWidth - scaledHeight) / 2);
+                }
+            }
+            if (width > height) {
+                drag = 'leftRight';
+                if (width > $('canvas').innerWidth()) {
+                    scaledHeight = $('canvas').innerHeight();
+                    scaledWidth = scaledHeight * aspectRat;
+                    posX = Math.ceil((scaledWidth - scaledHeight) / 2);
+                    origPosX = Math.ceil((scaledWidth - scaledHeight) / 2);
+                } else {
+                    scaledWidth = $('canvas').innerWidth();
+                    scaledHeight = scaledWidth / aspectRat;
+                    posY = Math.ceil((scaledHeight - scaledWidth) / 2);
+                    origPosY = Math.ceil((scaledHeight - scaledWidth) / 2);
+                }
+            }
+
+            $(document.body).on("keyup", function() {
+                var topText = top.val();
+                topText = topText.toUpperCase();
+
+                ctx.clearRect(0, 0, cvsWidth, cvsHeight);
+                ctx.drawImage(img, -1 * posX, -1 * posY, scaledWidth, scaledHeight);
+                ctx.textBaseline = 'top';
+                writeCaption(topText, 10);
+
+                $("<img />").load(function() {
+
+                    var img2 = this;
+
+                    ctx.globalAlpha = 0.9;
+                    ctx.drawImage(img2, 12, cvsHeight - 44, 306, 44);
+
+                }).attr("src", "assets/images/tagline.png");
+
+                CatInfo();
+
+            });
+
+            $(document.body).trigger("keyup");
+
+
+
+            var dragging = false;
+            var origOff = 0;
+
+
+            function handleDragStart(e) {
+                dragging = true;
+                if (drag == "leftRight") {
+                    if (e.offsetX) {
+                        origOff = e.offsetX;
+                    } else {
+                        origOff = e.originalEvent.touches[0].pageX
+                    }
+                } else {
+                    if (e.offsetY) {
+                        origOff = e.offsetY;
+                    } else {
+                        origOff = e.originalEvent.touches[0].pageY
+                    }
+                }
+            }
+
+
+            $('#canvas').mousedown(function(e) {
+                handleDragStart(e)
+            });
+            $('#canvas').bind('touchstart', function(e) {
+                handleDragStart(e)
+            });
+
+            function handleDragMove(e) {
+                e.preventDefault();
+                if (dragging) {
+                    if (drag === "leftRight") {
+                        if (e.offsetX) {
+                            draggedDist = (e.offsetX - origOff);
+                            origOff = e.offsetX;
+                        } else {
+                            draggedDist = (e.originalEvent.touches[0].pageX - origOff);
+                            origOff = e.originalEvent.touches[0].pageX;
+                        }
+
+                        if ((posX - draggedDist) > 0 && (posX - draggedDist) < (scaledWidth - scaledHeight)) {
+                            posX = posX - draggedDist;
+                        }
+                        if (posX < 0) posX = 0;
+                        if (posX > (scaledWidth - scaledHeight)) posX = scaledWidth - scaledHeight;
+                    } else {
+                        if (e.offsetY) {
+                            draggedDist = (e.offsetY - origOff);
+                            origOff = e.offsetY;
+                        } else {
+                            draggedDist = (e.originalEvent.touches[0].pageY - origOff);
+                            origOff = e.originalEvent.pageY;
+                        }
+                        if ((posY - draggedDist) > 0 && (posY - draggedDist) < (scaledHeight - scaledWidth)) {
+                            posY = posY - draggedDist;
+                        }
+                        if (posY < 0) posY = 0;
+                        if (posY > (scaledHeight - scaledWidth)) posX = scaledHeight - scaledWidth;
+                    }
+                    var topText = top.val();
+                    topText = topText.toUpperCase();
+                    ctx.clearRect(0, 0, cvsWidth, cvsHeight);
+                    ctx.drawImage(img, -1 * posX, -1 * posY, scaledWidth, scaledHeight);
+                    ctx.textBaseline = 'top';
+                    writeCaption(topText, 10);
+                }
+            }
+
+            $('#canvas').mousemove(function(e) {
+                handleDragMove(e)
+            });
+            $('#canvas').bind('touchmove', function(e) {
+                handleDragMove(e)
+            });
+
+            $('#canvas').mouseup(function(e) {
+                dragging = false;
+            });
+            $("#canvas").mouseout(function(e) {
+                dragging = false;
+            });
+            $(document).bind('touchend', function(e) {
+                dragging = false;
+            });
+
+
+        });
+
+    }
+
+});
+
+function tweet() {
+
+}
+
+function validateEmail(email) { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+} 
+
+$('#email button').click(function() {
+    if(!validateEmail($('#email-input').val())) {
+        return false;
+    } else {
+        return true;
+    }
+    return false;
+});
+
+function dlCanvas() {
+    var dt = $("canvas")[0].toDataURL('image/png');
+    this.href = dt;
+    _gaq.push(['_trackEvent', "Cat saves Tiger", "Meme downloaded"]);
+    _gaq.push(['account2._trackEvent', "Cat saves Tiger", "Meme downloaded"]);
+    _gaq.push(['account3._trackEvent', "Cat saves Tiger", "Meme downloaded"]);
+};
+dl.addEventListener('click', dlCanvas, false);
---
+++ assets/js/social-post.js
@@ -0,0 +1,163 @@
+﻿
+
+if (XMLHttpRequest.prototype.sendAsBinary === undefined) {
+    XMLHttpRequest.prototype.sendAsBinary = function (string) {
+        var bytes = Array.prototype.map.call(string, function (c) {
+            return c.charCodeAt(0) & 0xff;
+        });
+        this.send(new Uint8Array(bytes).buffer);
+    };
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/all.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: "910783989042597",
+        status: true,
+        cookie: true,
+        xfbml: true  // parse XFBML
+    });
+};
+
+function successSharing(platform) {
+    $('#sharesuccess').html("Successfuly shared to "+ platform);
+    $('#sharesuccess').fadeIn();
+    $('#form').css('height', $('#form').outerHeight() + 40 + "px")
+}
+
+function postImageToFacebook(authToken, filename, mimeType, imageData, message) {
+    // this is the multipart/form-data boundary we'll use
+    var boundary = '----ThisIsTheBoundary1234567890';
+    // let's encode our image file, which is contained in the var
+    var formData = '--' + boundary + '\r\n'
+    formData += 'Content-Disposition: form-data; name="source"; filename="' + filename + '"\r\n';
+    formData += 'Content-Type: ' + mimeType + '\r\n\r\n';
+    for (var i = 0; i < imageData.length; ++i) {
+        formData += String.fromCharCode(imageData[i] & 0xff);
+    }
+    formData += '\r\n';
+    formData += '--' + boundary + '\r\n';
+    formData += 'Content-Disposition: form-data; name="message"\r\n\r\n';
+    formData += message + '\r\n'
+    formData += '--' + boundary + '--\r\n';
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://graph.facebook.com/me/photos?access_token=' + authToken, true);
+    xhr.onload = xhr.onerror = function () {
+        //console.log(xhr.responseText);
+    };
+    xhr.setRequestHeader("Content-Type", "multipart/form-data; boundary=" + boundary);
+    xhr.sendAsBinary(formData);
+    xhr.onreadystatechange=function() {
+        //console.log(xhr);
+        if (xhr.readyState==4 && xhr.status==200) {
+            successSharing('Facebook');
+            //console.log('succ');
+            //document.getElementById("myDiv").innerHTML=xhr.responseText;
+        }
+    }
+};
+
+function postImageToTwitterProxy(filename, mimeType, imageData, message) {
+    // this is the multipart/form-data boundary we'll use
+    var boundary = '----ThisIsTheBoundary1234567890';
+    // let's encode our image file, which is contained in the var
+    var formData = '--' + boundary + '\r\n'
+    formData += 'Content-Disposition: form-data; name="source"; filename="' + filename + '"\r\n';
+    formData += 'Content-Type: ' + mimeType + '\r\n\r\n';
+    for (var i = 0; i < imageData.length; ++i) {
+        formData += String.fromCharCode(imageData[i] & 0xff);
+    }
+    formData += '\r\n';
+    formData += '--' + boundary + '\r\n';
+    formData += 'Content-Disposition: form-data; name="message"\r\n\r\n';
+    formData += message + '\r\n'
+    formData += '--' + boundary + '--\r\n';
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://dev.grnpc.org/index.php', true);
+    xhr.onload = xhr.onerror = function () {
+        //console.log(xhr.responseText);
+    };
+    xhr.setRequestHeader("Content-Type", "multipart/form-data; boundary=" + boundary);
+    xhr.sendAsBinary(formData);
+    xhr.onreadystatechange=function() {
+        //console.log(xhr);
+        if (xhr.readyState==4 && xhr.status==200) {
+            var response = JSON.parse(xhr.responseText);
+            if(response.loginRequired) {
+                window.location = response.loginRequired;
+            } else {
+                successSharing('Twitter');
+            }
+        }
+    }
+};
+
+var canvas;
+var context;
+var centerX;
+var img;
+var authToken ;
+
+function postCanvasToFacebook() {
+
+    canvas =$('canvas')[0]
+    //var data = $('canvas')[0].toDataURL("image/png");
+    var data = canvas.toDataURL("image/png");
+
+    var encodedPng = data.substring(data.indexOf(',') + 1, data.length);
+    var decodedPng = Base64Binary.decode(encodedPng);
+    FB.getLoginStatus(function (response) {
+
+
+        if (response.status === "connected") {
+            postImageToFacebook(response.authResponse.accessToken, "heroesgenerator", "image/png", decodedPng, $('#catname').val()+" saves TIGERS. Your cat can too. Visit www.greenpeace.org/Catssavetigers and spread the message on International Tiger Day.");
+        } else if (response.status === "not_authorized") {
+            FB.login(function (response) {
+                postImageToFacebook(response.authResponse.accessToken, "heroesgenerator", "image/png", decodedPng, $('#catname').val()+" saves TIGERS. Your cat can too. Visit www.greenpeace.org/Catssavetigers and spread the message on International Tiger Day.");
+            }, { scope: "publish_actions" });
+        } else {
+            FB.login(function (response) {
+                postImageToFacebook(response.authResponse.accessToken, "heroesgenerator", "image/png", decodedPng, $('#catname').val()+" saves TIGERS. Your cat can too. Visit www.greenpeace.org/Catssavetigers and spread the message on International Tiger Day.");
+            }, { scope: "publish_actions" });
+        }
+    
+    });
+
+};
+
+function postCanvasToTwitter() {
+
+    canvas =$('canvas')[0]
+    //var data = $('canvas')[0].toDataURL("image/png");
+    var data = canvas.toDataURL("image/png");
+
+    var encodedPng = data.substring(data.indexOf(',') + 1, data.length);
+    var decodedPng = Base64Binary.decode(encodedPng);
+    //var img = new Image();
+    //window.location.href = img.src = decodedPng;
+
+    postImageToTwitterProxy("heroesgenerator", "image/png", decodedPng, "JINGLES saves TIGERS. Your cat can too. www.greenpeace.org/Catssavetigers #CatsSaveTigers #Tigerday @GreenpeaceUK");
+
+};
+
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+var getParams = getUrlVars();
+if(getParams['tweeted']=='true') {
+    successSharing('Twitter');
+}
